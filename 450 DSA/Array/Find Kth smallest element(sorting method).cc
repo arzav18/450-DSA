cfe9cbdea54fffcb6987d1114c2c574b924fdb89
Arzav Jain <66370916+arzav18@users.noreply.github.com>
2022-02-01 01:46:22 +0530

    Add files via upload

---
+++ 450 DSA/Array/Find Kth smallest element(sorting method).cc
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int kthSmallest(int arr[],int n,int k)
+{
+    sort(arr,arr+n);
+    return arr[k-1];
+}
+
+int main()
+{
+
+    int arr[] = {4,2,6,22,66,33,88};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k;
+    cout<<"enter k:";
+    cin>>k;
+    cout<<"element is:"<<kthSmallest(arr,n,k);
+    return 0;
+}
---
+++ 450 DSA/Array/max & min in an array (linear method).cc
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+struct Pair
+{
+    int min;
+    int max;
+};
+
+Pair getMinMax(int arr[],int n)
+{
+    struct Pair minmax;
+    int i;
+
+    if(n==1)
+    {
+        minmax.min=arr[0];
+        minmax.max=arr[0];
+        return minmax;
+    }
+    if(arr[0]>arr[1])
+    {
+        minmax.min=arr[1];
+        minmax.max=arr[0];
+    }
+    else
+    {
+        minmax.min=arr[0];
+        minmax.max=arr[1];
+    }
+
+    for(i=2;i<n;i++)
+    {
+        if(arr[i]>minmax.max)
+        {
+            minmax.max=arr[i];
+        }
+        else if(arr[i]<minmax.min)
+        {
+            minmax.min=arr[i];
+        }
+    }
+    return minmax;
+}
+int main()
+{
+    int arr[] = {100,290,150,1,4,7};
+    int size = 6;
+
+    struct Pair minmax = getMinMax(arr,size);
+    cout<<"Minimum element is:"<<minmax.min<<endl;
+    cout<<"Maximum element is:"<<minmax.max<<endl;
+    return 0;
+}
