d2fcced552822c45a0d4e8a733e0452d06bc93e6
Arzav Jain <66370916+arzav18@users.noreply.github.com>
2022-02-09 23:37:37 +0530

    Update Trapping rain water(Optimal).cc

--- 450 DSA/Array/Trapping rain water(Optimal).cc
+++ 450 DSA/Array/Trapping rain water(Optimal).cc
@@ -1,1 +1,34 @@
+//T.C = O(N) S.C = O(1)
 
+int low = 0;
+int high = n-1;
+int mi = 0;
+int ma = 0;
+int ans = 0;
+while(low<=high)
+{
+  if(arr[low]<arr[high])
+  {
+    if(arr[low]>mi)
+    {
+      mi = arr[low];
+    }
+    else
+    {
+      ans += (mi-arr[low]);
+    }
+    low++;
+  }
+  else
+  {
+    if(arr[high]>ma)
+    {
+      ma = arr[high];
+    }
+    else
+    {
+      ans += ma-arr[high];
+    }
+    hi--;
+  }
+}
