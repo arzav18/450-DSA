b2df46c981ac78c47c25742b800fa9af3a6fd273
Arzav Jain <66370916+arzav18@users.noreply.github.com>
2022-02-26 23:22:57 +0530

    Add files via upload

---
+++ Recurssion/Print name 5 times.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void name(int i,int n)
+{
+    if(i>n)
+        return;
+
+    cout<<"Arzav"<<endl;
+    name(i+1,n);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    name(1,n);
+    return 0;
+}
