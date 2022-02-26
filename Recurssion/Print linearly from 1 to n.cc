5ef19b1857c482c74c3479f767c62fb6595526b1
Arzav Jain <66370916+arzav18@users.noreply.github.com>
2022-02-26 23:22:05 +0530

    Add files via upload

---
+++ Recurssion/Print linearly from 1 to n.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void printNumber(int i,int n)
+{
+    if(i>n)
+        return;
+    cout<<i<<" ";
+    printNumber(i+1,n);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    printNumber(1,n);
+    return 0;
+}
