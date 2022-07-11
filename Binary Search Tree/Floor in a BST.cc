b3f971ed374e016f1ddd852e51999bb86401bb60
Arzav Jain <66370916+arzav18@users.noreply.github.com>
2022-07-11 17:46:02 +0530

    Create Floor in a BST.cc

---
+++ Binary Search Tree/Floor in a BST.cc
@@ -0,0 +1,23 @@
+class Solution{
+  public:
+  int findFloor(Node<int> *root, int key)
+  {
+    int floor = -1;
+    while(root)
+    {
+      if(root->val == key)
+      {
+        floor = root->val;
+        return floor;
+      }
+      else if(root->val>key)
+        root = root->left;
+      else
+      {
+        floor = root->val;
+        root=root->right;
+      }
+    }
+    return floor;
+  }
+};
